c83ab1f199978eb4bef3d82906f3fc739d7ba572
Craig Alexander <craig.d.alexander@gmail.com>
2013-10-22 12:50:28 -0400

    Started on main settings page.

--- Damage/Damage/Damage/Scripts/Gadget.js
+++ Damage/Damage/Damage/Scripts/Gadget.js
@@ -1,4 +1,18 @@
-﻿function OpenSettingsDialog()
+﻿function OpenGadgetSettingsDialog() {
+    $('#gadgetSettingsDialog').dialog({
+        modal: true,
+        buttons: {
+            Save: function () {
+                $(this).dialog("close");
+            },
+            Cancel: function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
+
+function OpenSettingsDialog()
 {
     $('#settingsDialog').dialog({
         modal: true,
