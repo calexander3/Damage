8cfe6a0e3f5f08def527541a6d16e09ec4c2bf7a
Craig Alexander <craig.d.alexander@gmail.com>
2013-10-21 22:00:53 -0400

    Started add gadget dialog

--- Damage/Damage/Damage/Scripts/Gadget.js
+++ Damage/Damage/Damage/Scripts/Gadget.js
@@ -12,3 +12,18 @@
         }
     });
 }
+
+
+function OpenAddGadgetDialog() {
+    $('#addGadgetDialog').dialog({
+        modal: true,
+        buttons: {
+            Add: function () {
+                $(this).dialog("close");
+            },
+            Cancel: function () {
+                $(this).dialog("close");
+            }
+        }
+    });
+}
