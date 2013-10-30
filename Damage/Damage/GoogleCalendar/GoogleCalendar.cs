660fb156a1ef6fa4bd466538c0780c4376ebca1b
Craig Alexander <craig.d.alexander@gmail.com>
2013-10-29 20:45:52 -0400

    started calendar

--- Damage/Damage/GoogleCalendar/GoogleCalendar.cs
+++ Damage/Damage/GoogleCalendar/GoogleCalendar.cs
@@ -2,21 +2,28 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GoogleCalendar
 {
-    public class GoogleCalendar: IGadget
+    public class GoogleCalendar : IGadget
     {
+        string _output = "";
         public void Initialize()
         {
+            var request = (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/calendar/v3/calendars/craig.d.alexander@gmail.com/events?timeMin=2013-04-26T00:00:00Z&access_token=ya29.AHES6ZRhOr8kzt7lO0D-BdQJZSMZX-VZIewKRzOgsXPbKbO-yOjCGA");
+            using (var response = request.GetResponse())
+            {
+                _output = new System.IO.StreamReader(response.GetResponseStream()).ReadToEnd();
+            }
 
         }
 
         public string HTML
         {
-            get { return ""; }
+            get { return _output; }
         }
 
         public string Title
