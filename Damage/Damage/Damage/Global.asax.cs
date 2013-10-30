7908baff1e030a2b02efd817639b48d5c85daf62
Craig Alexander <craig.d.alexander@gmail.com>
2013-10-29 20:13:15 -0400

    https

--- Damage/Damage/Damage/Global.asax.cs
+++ Damage/Damage/Damage/Global.asax.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Web;
+using System;
 
 namespace Damage
 {
@@ -18,6 +20,14 @@ namespace Damage
 
     public class MvcApplication : System.Web.HttpApplication
     {
+        protected void Application_BeginRequest(Object sender, EventArgs e)
+        {
+            if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
+            {
+                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] + HttpContext.Current.Request.RawUrl);
+            }
+        }
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
