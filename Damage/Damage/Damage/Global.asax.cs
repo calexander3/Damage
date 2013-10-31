9f1ccc921078aaa57abd6c0be8901f78dc60eb77
Craig Alexander <craig.d.alexander@gmail.com>
2013-10-30 22:19:26 -0400

    Trust level

--- Damage/Damage/Damage/Global.asax.cs
+++ Damage/Damage/Damage/Global.asax.cs
@@ -20,13 +20,13 @@ namespace Damage
 
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_BeginRequest(Object sender, EventArgs e)
-        {
-            if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
-            {
-                Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] + HttpContext.Current.Request.RawUrl);
-            }
-        }
+        //protected void Application_BeginRequest(Object sender, EventArgs e)
+        //{
+        //    if (HttpContext.Current.Request.IsSecureConnection.Equals(false) && HttpContext.Current.Request.IsLocal.Equals(false))
+        //    {
+        //        Response.Redirect("https://" + Request.ServerVariables["HTTP_HOST"] + HttpContext.Current.Request.RawUrl);
+        //    }
+        //}
 
         protected void Application_Start()
         {
