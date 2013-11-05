e4fe80114829984060f84af8cdf9c5bcd22793ae
Craig Alexander <craig.d.alexander@gmail.com>
2013-11-04 20:13:27 -0500

    Sped up gmail

--- Damage/Damage/Damage/Controllers/GmailController.cs
+++ Damage/Damage/Damage/Controllers/GmailController.cs
@@ -27,7 +27,7 @@ namespace Damage.Controllers
                     if (DateTime.Compare(DateTime.Now, user.OAuthAccessTokenExpiration) < 0)
                     {
                         var client = new ImapX.ImapClient("imap.gmail.com", true, true);
-                        client.Behavior.MessageFetchMode = MessageFetchMode.Basic | MessageFetchMode.GMailExtendedData | MessageFetchMode.InternalDate;
+                        client.Behavior.MessageFetchMode = MessageFetchMode.Headers | MessageFetchMode.Tiny | MessageFetchMode.GMailExtendedData | MessageFetchMode.InternalDate;
                         if (client.Connect())
                         {
                             var credentials = new OAuth2Credentials(user.EmailAddress, user.CurrentOAuthAccessToken);
@@ -35,24 +35,40 @@ namespace Damage.Controllers
                             {
                                 if (client.IsAuthenticated)
                                 {
-                                    var _messages = client.Folders.Single(f => f.Name.ToLower() == folderName.ToLower()).Search((showUnreadOnly ? "UNSEEN" : "ALL")).OrderByDescending(m => (m.InternalDate ?? m.Date).Value).ToList();
+                                    var _messages = client.Folders.Single(f => f.Name.ToLower() == folderName.ToLower()).Search((showUnreadOnly ? "UNSEEN" : "ALL"), client.Behavior.MessageFetchMode, 100).OrderByDescending(m => (m.InternalDate ?? m.Date).Value).ToList();
+                                    var threads = new Dictionary<long, ImapX.Message>();
+                                    var threadCounts = new Dictionary<long, int>();
                                     foreach (var m in _messages)
                                     {
-                                        var messageDate =  (timezoneOffset.HasValue ? m.InternalDate.Value.AddMinutes(timezoneOffset.Value) : m.InternalDate.Value.AddMinutes(timezoneOffset.Value));
-                                        var messageDateString = (DateTime.Compare(messageDate.Date,DateTime.Now.Date) == 0 ? messageDate.ToShortTimeString() : messageDate.ToShortDateString());
+                                        if (!threads.ContainsKey(m.GmailThread.Id))
+                                        {
+                                            threads.Add(m.GmailThread.Id, m);
+                                            threadCounts.Add(m.GmailThread.Id, 1);
+                                        }
+                                        else
+                                        {
+                                            threadCounts[m.GmailThread.Id] += 1;
+                                        }
+                                    }
+
+                                    foreach (var thread in threads)
+                                    {
+                                        var messageDate = (timezoneOffset.HasValue ? thread.Value.InternalDate.Value.AddMinutes(timezoneOffset.Value) : thread.Value.InternalDate.Value.AddMinutes(timezoneOffset.Value));
+                                        var messageDateString = (DateTime.Compare(messageDate.Date, DateTime.Now.Date) == 0 ? messageDate.ToShortTimeString() : messageDate.ToShortDateString());
                                         output.Add(new GmailMessage()
                                         {
-                                            Subject = m.Subject,
-                                            From = (m.From.DisplayName.Length > 0 ? m.From.DisplayName : m.From.Address),
-                                            MessageIdHex = m.GmailThread.Id.ToString("X").ToLower(),
-                                            MessageId =  m.GmailThread.Id,
-                                            ImapId = m.UId,
+                                            Subject = thread.Value.Subject,
+                                            From = (thread.Value.From.DisplayName.Length > 0 ? thread.Value.From.DisplayName : thread.Value.From.Address) + (threadCounts[thread.Key] > 1 ? " (" +  threadCounts[thread.Key] + ")" : ""),
+                                            MessageIdHex = thread.Value.GmailThread.Id.ToString("X").ToLower(),
+                                            MessageId = thread.Value.GmailThread.Id,
+                                            ImapId = thread.Value.UId,
                                             Date = messageDateString,
-                                            Preview = getPreview(m.Body),
-                                            Unread = !m.Seen,
-                                            Important = (m.Labels.Count() > 0 && m.Labels[0].Equals(""))
+                                            Preview = getPreview(thread.Value.Body),
+                                            Unread = !thread.Value.Seen,
+                                            Important = (thread.Value.Labels.Count() > 0 && thread.Value.Labels[0].Equals(""))
                                         });
                                     }
+
                                 }
                                 successful = true;
                             }
@@ -63,7 +79,8 @@ namespace Damage.Controllers
             return Json(new { Result = successful, Data = output }, JsonRequestBehavior.AllowGet);
         }
 
-        public JsonResult DeleteMail(string MessageId)
+        [HttpPost]
+        public JsonResult DeleteMail(string[] MessageIds)
         {
             var successful = false;
             if (Request.IsAuthenticated)
@@ -75,7 +92,7 @@ namespace Damage.Controllers
                     if (DateTime.Compare(DateTime.Now, user.OAuthAccessTokenExpiration) < 0)
                     {
                         var client = new ImapX.ImapClient("imap.gmail.com", true, true);
-                        client.Behavior.MessageFetchMode = MessageFetchMode.Basic | MessageFetchMode.GMailExtendedData | MessageFetchMode.InternalDate;
+                        client.Behavior.MessageFetchMode = MessageFetchMode.Headers | MessageFetchMode.GMailExtendedData | MessageFetchMode.InternalDate;
                         if (client.Connect())
                         {
                             var credentials = new OAuth2Credentials(user.EmailAddress, user.CurrentOAuthAccessToken);
@@ -83,7 +100,12 @@ namespace Damage.Controllers
                             {
                                 if (client.IsAuthenticated)
                                 {
-                                    var _message = client.Folders.Single(f => f.Name.ToLower() == "inbox").Search("uid " + MessageId, MessageFetchMode.GMailExtendedData).Single();
+                                    var MessageIdSequence = "";
+                                    foreach (var messageId in MessageIds)
+                                    {
+                                        MessageIdSequence += messageId + " ";
+                                    }
+                                    var _message = client.Folders.Single(f => f.Name.ToLower() == "inbox").Search("uid " + MessageIdSequence, MessageFetchMode.GMailExtendedData).Single();
                                     successful = _message.Remove();
                                 }
                             }
@@ -92,20 +114,19 @@ namespace Damage.Controllers
                 }
             }
 
-            return Json(new { Result = successful }, JsonRequestBehavior.AllowGet);
+            return Json(new { Result = successful });
         }
 
         private string getPreview(MessageBody body)
         {
             if (body.HasText)
             {
-                var text = Regex.Replace(body.Text, "<.*?>", "", RegexOptions.Singleline);
-                return (text.Length > 80 ? text.Substring(0, 80) : text);
+                return HttpUtility.HtmlEncode(body.Text.Length > 80 ? body.Text.Substring(0, 80) : body.Text);
             }
             else if (body.HasHtml)
             {
                 var text = Regex.Replace(body.Html, "<.*?>", "", RegexOptions.Singleline);
-                return (text.Length > 80 ? text.Substring(0, 80) : text);
+                return HttpUtility.HtmlEncode(text.Length > 80 ? text.Substring(0, 80) : text);
             }
             else
             {
