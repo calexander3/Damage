ebf0f15fa1571e04c988b87d63da2b7505170835
Craig Alexander <craig.d.alexander@gmail.com>
2013-11-17 00:46:08 -0500

    Added google voice project.

---
+++ Damage/Damage/GoogleVoice/GoogleVoice.cs
@@ -0,0 +1,52 @@
+﻿using Damage.Gadget;
+using System.Collections.Generic;
+
+
+namespace GoogleVoice
+{
+    public class GoogleVoice : IGadget
+    {
+        string _output = "";
+        public void Initialize()
+        {
+
+        }
+
+        public string HTML
+        {
+            get { return _output; }
+        }
+
+        public string Title
+        {
+            get { return "Google Voice"; }
+        }
+
+        public string Description
+        {
+            get { return "View your Google Voice messages."; }
+        }
+
+        public string DefaultSettings
+        {
+            get { return ""; }
+        }
+
+        public List<GadgetSettingField> SettingsSchema
+        {
+            get { return new List<GadgetSettingField>(); }
+        }
+
+        public Damage.DataAccess.Models.UserGadget UserGadget { get; set; }
+
+        public bool InBeta
+        {
+            get { return true; }
+        }
+
+        public bool RequiresValidGoogleAccessToken
+        {
+            get { return true; }
+        }
+    }
+}
