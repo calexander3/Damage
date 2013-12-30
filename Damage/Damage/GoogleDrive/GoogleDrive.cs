c15b844147cdc756e3fd1193c8bb3b28660174ec
Craig Alexander <craig.d.alexander@gmail.com>
2013-12-30 17:57:56 -0500

    Started working on google drive gadget.

--- Damage/Damage/GoogleDrive/GoogleDrive.cs
+++ Damage/Damage/GoogleDrive/GoogleDrive.cs
@@ -1,5 +1,9 @@
-﻿using Damage.Gadget;
+﻿using System;
+using System.Globalization;
+using System.Net;
+using Damage.Gadget;
 using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace GoogleDrive
 {
@@ -8,7 +12,30 @@ namespace GoogleDrive
         string _output = "";
         public void Initialize()
         {
+            var outputBuilder = new System.Text.StringBuilder("<div>");
 
+
+            outputBuilder.Append("<div><img src='/Content/Images/GoogleDriveDocument.png'><a href='https://docs.google.com/document/' target='_blank'>Create New Document</a></div>");
+
+            UsageInfo usageInfo = null;
+            var usageRequest = (HttpWebRequest) WebRequest.Create("https://www.googleapis.com/drive/v2/about");
+            usageRequest.Headers["Authorization"] = string.Format("Bearer {0}", UserGadget.User.CurrentOAuthAccessToken);
+            using (var usageResponse = usageRequest.GetResponse())
+            {
+                usageInfo = JsonConvert.DeserializeObject<UsageInfo>(new System.IO.StreamReader(usageResponse.GetResponseStream()).ReadToEnd());
+            }
+
+            if (usageInfo != null)
+            {
+                outputBuilder.Append("<div>" +
+                                     Math.Round(
+                                         ((double.Parse(usageInfo.quotaBytesUsedAggregate)/
+                                           double.Parse(usageInfo.quotaBytesTotal))*100), 2) + "% Space Used</div>");
+            }
+
+
+            outputBuilder.Append("</div>");
+            _output = outputBuilder.ToString();
         }
 
         public string HTML
@@ -40,7 +67,7 @@ namespace GoogleDrive
 
         public bool InBeta
         {
-            get { return true; }
+            get { return false; }
         }
 
         public bool RequiresValidGoogleAccessToken
---
+++ Damage/Damage/GoogleDrive/UsageInfo.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoogleDrive
+{
+    public class Picture
+    {
+        public string url { get; set; }
+    }
+
+    public class User
+    {
+        public string kind { get; set; }
+        public string displayName { get; set; }
+        public Picture picture { get; set; }
+        public bool isAuthenticatedUser { get; set; }
+        public string permissionId { get; set; }
+    }
+
+    public class ImportFormat
+    {
+        public string source { get; set; }
+        public List<string> targets { get; set; }
+    }
+
+    public class ExportFormat
+    {
+        public string source { get; set; }
+        public List<string> targets { get; set; }
+    }
+
+    public class AdditionalRoleInfo
+    {
+        public string type { get; set; }
+        public List<object> roleSets { get; set; }
+    }
+
+    public class Feature
+    {
+        public string featureName { get; set; }
+        public double? featureRate { get; set; }
+    }
+
+    public class MaxUploadSize
+    {
+        public string type { get; set; }
+        public string size { get; set; }
+    }
+
+    public class UsageInfo
+    {
+        public string kind { get; set; }
+        public string etag { get; set; }
+        public string selfLink { get; set; }
+        public string name { get; set; }
+        public User user { get; set; }
+        public string quotaBytesTotal { get; set; }
+        public string quotaBytesUsed { get; set; }
+        public string quotaBytesUsedAggregate { get; set; }
+        public string quotaBytesUsedInTrash { get; set; }
+        public string largestChangeId { get; set; }
+        public string rootFolderId { get; set; }
+        public string domainSharingPolicy { get; set; }
+        public string permissionId { get; set; }
+        public List<ImportFormat> importFormats { get; set; }
+        public List<ExportFormat> exportFormats { get; set; }
+        public List<AdditionalRoleInfo> additionalRoleInfo { get; set; }
+        public List<Feature> features { get; set; }
+        public List<MaxUploadSize> maxUploadSizes { get; set; }
+        public bool isCurrentAppInstalled { get; set; }
+    }
+}
